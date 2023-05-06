ec504c1443dccad533baf35233ac88e46f39d75d
joabe levi <joabe-levi@hotmail.com>
2023-05-06 00:11:37 -0300

    - Inventario    - Feito inclusão de urls para urls do projeto    - Feito inclusão de urls do CRUD de pessoas

--- inventario/urls.py
+++ inventario/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import PessoaViewSet, ObjetoViewSet, PossePessoaObjetoViewSet
+from . import views
 
 router = routers.DefaultRouter()
-router.register(r'pessoas', PessoaViewSet)
-router.register(r'objetos', ObjetoViewSet)
-router.register(r'posses', PossePessoaObjetoViewSet)
+router.register(r'objetos', views.ObjetoViewSet)
+router.register(r'posses', views.PossePessoaObjetoViewSet)
 
-urlpatterns = [
-    path('', include(router.urls)),
+URLS_PESSOAS = [
+    path('api/pessoas/', views.PessoaListView.as_view(), name='pessoas-list'),
+    path('api/pessoas/incluir/', views.PessoaCreateView.as_view(), name='pessoas-create'),
+    path('api/pessoas/atualizar/<int:pk>/', views.PessoaUpdateView.as_view(), name='pessoas-update'),
+    path('api/pessoas/excluir/<int:pk>/', views.PessoaDeleteView.as_view(), name='pessoas-update'),
+]
+
+urlpatterns = URLS_PESSOAS + [
+    path('', views.IndexView.as_view()),
+    path('overview/', views.ApiOverview.as_view(), name='overview'),
+    path('api/', include(router.urls)),
 ]
