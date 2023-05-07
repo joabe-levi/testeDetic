a61f9e323cb91a4f5dee48d8b7132c2250a1d160
joabe levi <joabe-levi@hotmail.com>
2023-05-06 22:56:37 -0300

    - Inventario    - Adicionando novos modelos de Arma e Registro de Arma    - Em modelo de Pessoa, feito mecanica de limpar cpf caso contenha pontos e traços    - Adicionando post_save ao modelo de Objeto para criar arma caso a mesma seja do tipo ARMA    -

--- inventario/models.py
+++ inventario/models.py
@@ -1,8 +1,7 @@
-from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
 import re
-
 from utils.basics.models import BasicModel
 from . import choices
 
@@ -16,6 +15,18 @@ class Pessoa(User):
         verbose_name = 'Pessoa'
         verbose_name_plural = 'Pessoas'
         db_table = 'pessoa'
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def limpa_cpf(self):
+        return self.cpf.replace('-', '').replace('.', '')
+
+    def save(self, *args, **kwargs):
+        if not self.cpf.isnumeric():
+            self.cpf = self.limpa_cpf()
+
+        super().save()
 
     @staticmethod
     def valida_cpf(cpf):
@@ -56,20 +67,53 @@ class Objeto(BasicModel):
         verbose_name_plural = 'Objetos'
         db_table = 'objeto'
 
+    def __str__(self):
+        return f'{self.descricao} - {self.get_tipo_display()}'
+
+
+def consultar_arma(sender, **kwargs):
+    instance = kwargs['instance']
+    if instance.tipo == choices.CHOICE_ARMA:
+        Arma.objects.get_or_create(objeto=instance)
+
+
+post_save.connect(consultar_arma, sender=Objeto)
+
 
 class PossePessoaObjeto(BasicModel):
     pessoa = models.ForeignKey(Pessoa, related_name='posses_por_pessoa', on_delete=models.PROTECT)
     objeto = models.ForeignKey(Objeto, related_name='posses_por_pessoa', on_delete=models.PROTECT)
-    eh_de_policia = models.BooleanField(default=False)
 
     class Meta:
         unique_together = ('pessoa', 'objeto')
 
         verbose_name = 'Posse da Pessoa'
-        verbose_name_plural = 'Posses da Pessoa'
+        verbose_name_plural = 'Posses de Pessoas'
         db_table = 'posse_pessoa_objeto'
 
-    def save(self, *args, **kwargs):
-        if self.eh_de_policia and not self.pessoa.eh_policial:
-            raise ValidationError('Somente policiais devem possuir objetos desse tipo!')
-        super().save(*args, **kwargs)
+    def __str__(self):
+        return f'{self.objeto} - {self.pessoa}'
+
+
+class Arma(BasicModel):
+    objeto = models.ForeignKey(Objeto, related_name='armas', on_delete=models.PROTECT)
+
+    class Meta:
+        verbose_name = 'Arma'
+        verbose_name_plural = 'Armas'
+        db_table = 'arma'
+
+    def __str__(self):
+        return f'{self.objeto}'
+
+
+class RegistroOficialDeArma(BasicModel):
+    arma = models.OneToOneField(Arma, related_name='registros_oficiais_armas', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Registro Oficial de Arma'
+        verbose_name_plural = 'Registros Oficiais de Armas'
+        db_table = 'registro_oficial_arma'
+
+    def __str__(self):
+        return f'{self.arma}'
