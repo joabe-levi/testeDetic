9f8ddf14c2099d5d4db135a20d0585fbbc8d8cce
joabe levi <joabe-levi@hotmail.com>
2023-05-06 22:58:39 -0300

    - Inventario    - Adicionando novas views, feito ajustes nas outras    - Removendo funções de atualizar parcialmente pois nas bases dos recursos já faziam isso

--- inventario/views.py
+++ inventario/views.py
@@ -8,7 +8,7 @@ from rest_framework import status, filters
 from rest_framework.views import APIView
 from django_filters.rest_framework import DjangoFilterBackend
 
-from .filters import PessoaFilter
+from . import filters as filter_inventario
 from .models import Pessoa, Objeto, PossePessoaObjeto
 from .serializers import PessoaSerializer, ObjetoSerializer, PossePessoaObjetoSerializer
 
@@ -25,27 +25,25 @@ class ApiOverview(APIView):
     Lista de todos os endpoints disponiveis
     """
 
-    def get(self, request, format=None):
+    def get(self, request, *args, **kwargs):
         api_urls = {
             'Objetos': {
                 'list': 'api/objetos/',
-                # 'detail': 'api/objetos/<int:pk>/',
                 'create': 'api/objetos/incluir/',
-                'update': 'api/objetos/<int:pk>/update/',
-                'delete': 'api/objetos/<int:pk>/delete/',
+                'update': 'api/objetos/atualizar/<int:pk>/',
+                'delete': 'api/objetos/excluir/<int:pk>/',
             },
             'Pessoas': {
                 'list': 'api/pessoas/',
-                'create': 'api/pessoas/create/',
+                'create': 'api/pessoas/incluir/',
                 'update': 'api/pessoas/atualizar/<int:pk>',
                 'delete': 'api/pessoas/excluir/<int:pk>/',
             },
             'PossePessoaObjeto': {
-                'list': 'api/posse_pessoa_objetos/',
-                'detail': 'api/posse_pessoa_objetos/<int:pk>/',
-                'create': 'api/posse_pessoa_objetos/create/',
-                'update': 'api/posse_pessoa_objetos/<int:pk>/update/',
-                'delete': 'api/posse_pessoa_objetos/<int:pk>/delete/',
+                'list': 'api/posses/',
+                'create': 'api/posses/incluir/',
+                'update': 'api/posses/atualizar/<int:pk>/',
+                'delete': 'api/posses/excluir/<int:pk>/',
             }
         }
 
@@ -55,16 +53,9 @@ class ApiOverview(APIView):
 class PessoaListView(generics.ListAPIView):
     queryset = Pessoa.objects.all()
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
-    filterset_class = PessoaFilter
+    filterset_class = filter_inventario.PessoaFilter
     serializer_class = PessoaSerializer
     permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        filter_qs = dict()
-        if self.request.query_params:
-            filter_qs = self.request.query_params.dict()
-
-        return Pessoa.objects.filter(**filter_qs)
 
 
 class PessoaCreateView(generics.CreateAPIView):
@@ -72,9 +63,8 @@ class PessoaCreateView(generics.CreateAPIView):
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
-        cpf = serializer.data.get('cpf')
+        cpf = serializer.validated_data.get('cpf')
         if Pessoa.valida_cpf(cpf):
-            # TODO Posteriormente criar mecanica de limpar cpf para remoção de caracteres
             super().perform_create(serializer)
 
 
@@ -85,9 +75,6 @@ class PessoaUpdateView(generics.UpdateAPIView):
     def get_object(self):
         return Pessoa.objects.get(pk=self.kwargs['pk'])
 
-    def put(self, request, *args, **kwargs):
-        return self.partial_update(request, *args, **kwargs)
-
 
 class PessoaDeleteView(generics.DestroyAPIView):
     serializer_class = PessoaSerializer
@@ -97,24 +84,55 @@ class PessoaDeleteView(generics.DestroyAPIView):
         return Pessoa.objects.get(pk=self.kwargs['pk'])
 
 
-
-
-class ObjetoViewSet(viewsets.ModelViewSet):
+#############################################################################
+class ObjetoListView(generics.ListAPIView):
     queryset = Objeto.objects.all()
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_class = filter_inventario.ObjetoFilter
     serializer_class = ObjetoSerializer
     permission_classes = [IsAuthenticated]
 
 
-class PossePessoaObjetoViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin,
-                               mixins.RetrieveModelMixin, mixins.UpdateModelMixin,
-                               mixins.DestroyModelMixin, mixins.ListModelMixin):
+class ObjetoCreateView(generics.CreateAPIView):
+    serializer_class = ObjetoSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class ObjetoUpdateView(generics.UpdateAPIView):
+    serializer_class = ObjetoSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return Objeto.objects.get(id=self.kwargs.get('pk'))
+
+
+class ObjetoDeleteView(generics.DestroyAPIView):
+    serializer_class = PessoaSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return Objeto.objects.get(pk=self.kwargs['pk'])
+
+
+#############################################################################
+class PossePessoaObjetoListView(generics.ListAPIView):
     queryset = PossePessoaObjeto.objects.all()
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_class = filter_inventario.PossePorPessoaFilter
     serializer_class = PossePessoaObjetoSerializer
     permission_classes = [IsAuthenticated]
 
-    @action(detail=True, methods=['post'])
-    def transferir_posse(self, request, pk=None):
-        posse = self.get_object()
-        posse.eh_de_policia = not posse.eh_de_policia
-        posse.save()
-        return Response(self.serializer_class(posse).data)
+
+class PossePessoaObjetoCreateView(generics.CreateAPIView):
+    serializer_class = PossePessoaObjetoSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class PossePessoaObjetoUpdateView(generics.UpdateAPIView):
+    serializer_class = ObjetoSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class PossePessoaObjetoDeleteView(generics.DestroyAPIView):
+    serializer_class = ObjetoSerializer
+    permission_classes = [IsAuthenticated]
