f29ae6c2a37e0f815f688abd1f9c0879155ca7c4
joabe levi <joabe-levi@hotmail.com>
2023-05-06 22:57:01 -0300

    - Inventario    - Adicionando regras de validação ao serializer

--- inventario/serializers.py
+++ inventario/serializers.py
@@ -1,21 +1,56 @@
 from rest_framework import serializers
-from .models import Pessoa, Objeto, PossePessoaObjeto
+from .models import Pessoa, Objeto, PossePessoaObjeto, RegistroOficialDeArma
+from . import choices
 
 
 class PessoaSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(required=False)
+    password = serializers.CharField(required=False)
+
     class Meta:
         model = Pessoa
         fields = '__all__'
-        read_only_fields = ('username', 'password')
 
 
 class ObjetoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Objeto
-        fields = '__all__'
+        fields = ('id', 'descricao', 'tipo', 'numero_serie', 'modelo', 'ano', 'cor',)
+        read_only_fields = ('id',)
 
 
 class PossePessoaObjetoSerializer(serializers.ModelSerializer):
     class Meta:
         model = PossePessoaObjeto
-        fields = '__all__'
+        fields = ('objeto', 'pessoa', 'eh_de_policia',)
+
+    def validate(self, data):
+        objeto = data.get('objeto')
+        pessoa = data.get('pessoa')
+
+        if objeto.tipo in [choices.CHOICE_CARRO, choices.CHOICE_VIATURA_POLICIAL]:
+            outras_posses = PossePessoaObjeto.objects.filter(objeto=objeto)
+
+            policiais_possuindo = outras_posses.filter(pessoa__eh_policial=True)
+
+            if objeto.tipo == choices.CHOICE_VIATURA_POLICIAL and not pessoa.eh_policial:
+                raise serializers.ValidationError(
+                    'Somente policiais civis podem possuir objetos do tipo viatura policial!'
+                )
+
+            if policiais_possuindo.count() >= 2:
+                raise serializers.ValidationError(
+                    'Apenas duas pessoas policiais civis podem possuir objetos do tipo viatura policial!'
+                )
+        if objeto.tipo == choices.CHOICE_ARMA:
+            if not RegistroOficialDeArma.objects.filter(arma__objeto=objeto).exists():
+                raise serializers.ValidationError('Esta arma não consta na tabela de registros oficiais!')
+
+        else:
+            outras_posses = PossePessoaObjeto.objects.filter(objeto=objeto)
+
+            if outras_posses.count() >= 1:
+                raise serializers.ValidationError(
+                    'Objetos dos demais tipos só podem ser possuídos por uma pessoa por vez!'
+                )
+        return data
