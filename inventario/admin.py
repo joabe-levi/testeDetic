d91e920d48178b3b1ca11869c0683955f3cf873e
joabe levi <joabe-levi@hotmail.com>
2023-05-06 22:54:36 -0300

    - Inventario    - Adicionando admin

--- inventario/admin.py
+++ inventario/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+admin.site.register(models.Pessoa)
+admin.site.register(models.Objeto)
+admin.site.register(models.PossePessoaObjeto)
+admin.site.register(models.RegistroOficialDeArma)
