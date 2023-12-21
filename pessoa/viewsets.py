0768334cd7c501a0cb03055caebd23140a67190f
joabe-levi <joabe-levi@hotmail.com>
2023-12-21 17:33:04 -0300

    - Pessoa    - Ajustes em viewset, herdando de base

--- pessoa/viewsets.py
+++ pessoa/viewsets.py
@@ -1,19 +1,16 @@
-from rest_framework.viewsets import ModelViewSet
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters
-from inventario import filters as filter_inventario
+from pessoa.filters import PessoaFilter
 from pessoa.models import Pessoa
 from pessoa.serializers import PessoaSerializer
-from utils.basics.permissions import IsPolicialOrReadOnly
 from rest_framework import status
 from rest_framework.response import Response
 
+from core.basics.viewsets.viewsets import BasicModelViewSet
 
-class PessoaViewSet(ModelViewSet):
-    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
-    filterset_class = filter_inventario.PessoaFilter
+
+class PessoaViewSet(BasicModelViewSet):
+    queryset = Pessoa.objects.all()
+    filterset_class = PessoaFilter
     serializer_class = PessoaSerializer
-    permission_classes = [IsPolicialOrReadOnly]
 
     def perform_create(self, serializer):
         cpf = serializer.validated_data.get('cpf')
