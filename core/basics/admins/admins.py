1f68b7393de51f4a4522b9ea433bf68d24aec6df
joabe-levi <joabe-levi@hotmail.com>
2023-12-28 13:18:25 -0300

    - Core    - Melhorando admin

--- core/basics/admins/admins.py
+++ core/basics/admins/admins.py
@@ -3,20 +3,16 @@ from django.contrib import admin
 from core.basics.models.managers import BasicManagerDefault
 
 
-def _atualizar_status_ativo(modeladmin, request, queryset):
-    queryset.update(ativo=True)
-
-
-def _atualizar_status_inativo(modeladmin, request, queryset):
-    queryset.update(ativo=False)
-
-
-_atualizar_status_ativo.short_description = 'Ativar registros'
-_atualizar_status_inativo.short_description = 'Desativar registros'
-
-
 class BasicAdmin(admin.ModelAdmin):
-    actions = [_atualizar_status_ativo, _atualizar_status_inativo]
+    actions = ['_atualizar_status_ativo', '_atualizar_status_inativo']
+
+    @admin.display(description='Desativar registros')
+    def _atualizar_status_inativo(self, request, queryset):
+        queryset.update(ativo=False)
+
+    @admin.display(description='Ativar registros')
+    def _atualizar_status_ativo(self, request, queryset):
+        queryset.update(ativo=True)
 
     def get_queryset(self, request):
         self.model.add_to_class('objects', BasicManagerDefault.as_manager())
