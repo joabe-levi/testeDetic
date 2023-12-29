ad8de5272226209481bf290c9e0ae621f4766641
joabe-levi <joabe-levi@hotmail.com>
2023-12-29 17:56:28 -0300

    - Core    - Melhorando permissões, adicionando um base    - Agora dessa maneira qualquer um pode visualizar os dados, somente quem tem permissão que poderá altera-los

--- core/basics/permissions/permissions.py
+++ core/basics/permissions/permissions.py
@@ -1,18 +1,30 @@
 from rest_framework import permissions
 
 
-class IsPolicialOrReadOnly(permissions.BasePermission):
+class BasicPermission(permissions.BasePermission):
+    REQUEST_METHODS_TO_READY = ('GET', 'HEAD', 'OPTIONS')
 
     def has_permission(self, request, view):
+        if request.method in self.REQUEST_METHODS_TO_READY:
+            return True
+
+
+class IsPolicialOrReadOnly(BasicPermission):
+
+    def has_permission(self, request, view):
+        super().has_permission(request, view)
+
         user = request.user
         if not hasattr(user, 'pessoa'):
             return False
         return user.is_authenticated and user.pessoa.eh_policial
 
 
-class IsSuperUserOrReadOnly(permissions.BasePermission):
+class IsSuperUserOrReadOnly(BasicPermission):
 
     def has_permission(self, request, view):
+        super().has_permission(request, view)
+
         user = request.user
         if not hasattr(user, 'pessoa'):
             return False
