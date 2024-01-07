f343df93bd5c1627ef3ddbdf3564e99019629be7
joabe levi <joabe-levi@hotmail.com>
2024-01-07 15:51:26 -0300

    - Core    - Melhorando permissions

--- core/basics/permissions/permissions.py
+++ core/basics/permissions/permissions.py
@@ -2,30 +2,27 @@ from rest_framework import permissions
 
 
 class BasicPermission(permissions.BasePermission):
-    REQUEST_METHODS_TO_READY = ('GET', 'HEAD', 'OPTIONS')
+    REQUEST_METHODS_TO_ALLOW = ('GET', 'HEAD', 'OPTIONS')
 
     def has_permission(self, request, view):
-        if request.method in self.REQUEST_METHODS_TO_READY:
-            return True
+        return request.method in self.REQUEST_METHODS_TO_ALLOW
 
 
 class IsPolicialOrReadOnly(BasicPermission):
 
     def has_permission(self, request, view):
-        super().has_permission(request, view)
+        if super().has_permission(request, view):
+            return True
 
         user = request.user
-        if not hasattr(user, 'pessoa'):
-            return False
-        return user.is_authenticated and user.pessoa.eh_policial
+        return user.is_authenticated and hasattr(user, 'pessoa') and user.pessoa.eh_policial
 
 
 class IsSuperUserOrReadOnly(BasicPermission):
 
     def has_permission(self, request, view):
-        super().has_permission(request, view)
+        if super().has_permission(request, view):
+            return True
 
         user = request.user
-        if not hasattr(user, 'pessoa'):
-            return False
         return user.is_authenticated and user.is_superuser
