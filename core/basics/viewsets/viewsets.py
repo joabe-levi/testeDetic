dc0ef518573bd52a18a8ac00bd9b31ec229ae752
joabe levi <joabe-levi@hotmail.com>
2024-01-07 15:52:55 -0300

    - Core    - Criando inteligencia para não precisar excluir registros do banco, apenas desativa-los.

--- core/basics/viewsets/viewsets.py
+++ core/basics/viewsets/viewsets.py
@@ -1,12 +1,25 @@
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
+from rest_framework import status
+from rest_framework.response import Response
 
 from core.basics.permissions.permissions import IsPolicialOrReadOnly
-
-from rest_framework import permissions
 
 
 class BasicModelViewSet(ModelViewSet):
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
     permission_classes = [IsPolicialOrReadOnly]
+    perform_destroy = False
+
+    def get_perform_destroy(self):
+        return self.perform_destroy
+
+    def destroy(self, request, *args, **kwargs):
+        if self.get_perform_destroy():
+            return super().destroy(request, *args, **kwargs)
+
+        instance = self.get_object()
+        instance.ativo = False
+        instance.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
