1fc0423b7733e93c7fef47a8924a15f7cd703942
joabe-levi <joabe-levi@hotmail.com>
2024-01-10 08:55:41 -0300

    - Pessoa    - Melhorando mecanica de criar uma pessoa, removendo do viewset e adicionando ao serializer

--- pessoa/serializers.py
+++ pessoa/serializers.py
@@ -9,3 +9,10 @@ class PessoaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pessoa
         fields = '__all__'
+
+    def create(self, validated_data):
+        cpf = validated_data.get('cpf')
+        if not Pessoa.valida_cpf(cpf):
+            raise serializers.ValidationError("Cpf informado não é valido!")
+
+        super().create(validated_data)
--- pessoa/viewsets.py
+++ pessoa/viewsets.py
@@ -11,19 +11,3 @@ class PessoaViewSet(BasicModelViewSet):
     queryset = Pessoa.objects.all()
     filterset_class = PessoaFilter
     serializer_class = PessoaSerializer
-
-    def perform_create(self, serializer):
-        cpf = serializer.validated_data.get('cpf')
-        if Pessoa.valida_cpf(cpf):
-            data = {
-                'message': 'Objeto criado com sucesso!',
-            }
-            status_data = status.HTTP_201_CREATED
-            super().perform_create(serializer)
-        else:
-            data = {
-                'message': 'Cpf informado não é valido!',
-            }
-            status_data = status.HTTP_400_BAD_REQUEST
-
-        return Response(data, status=status_data)
